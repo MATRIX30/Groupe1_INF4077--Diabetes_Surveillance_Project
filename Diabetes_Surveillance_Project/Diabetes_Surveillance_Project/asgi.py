186adac877feb5890415274b8d43d4c03f4718e9
MATRIX30 <MATRIXBOY30@GMAIL.COM>
2020-12-02 19:09:29 +0100

    Rename Project to match Context

--- Diabetes_Surveillance_Project/Diabetes_Surveillance_Project/asgi.py
+++ Diabetes_Surveillance_Project/Diabetes_Surveillance_Project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Diabetes_Surveillance_WebApp project.
+ASGI config for Diabetes_Surveillance_Project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Diabetes_Surveillance_WebApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Diabetes_Surveillance_Project.settings')
 
 application = get_asgi_application()
--- Diabetes_Surveillance_Project/Diabetes_Surveillance_Project/urls.py
+++ Diabetes_Surveillance_Project/Diabetes_Surveillance_Project/urls.py
@@ -1,4 +1,4 @@
-"""Diabetes_Surveillance_WebApp URL Configuration
+"""Diabetes_Surveillance_Project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
